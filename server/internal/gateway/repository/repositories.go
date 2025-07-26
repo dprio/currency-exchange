1fc867ae3931b820fc1aa8c498488cfcfdf57ed3
dprio <daniel.pedroso94@gmail.com>
2025-07-26 14:28:56 -0300

    feat: repositories

---
+++ server/internal/gateway/repository/repositories.go
@@ -0,0 +1,16 @@
+package repository
+
+import (
+	"github.com/dprio/currency-exchange/server/internal/gateway/repository/dollarquoterepository"
+	"github.com/dprio/currency-exchange/server/internal/infrastructure/db"
+)
+
+type Repositories struct {
+	DollarQuoteRepository dollarquoterepository.Repository
+}
+
+func New(db db.DB) Repositories {
+	return Repositories{
+		DollarQuoteRepository: dollarquoterepository.New(&db.DollarQuoteClient),
+	}
+}
