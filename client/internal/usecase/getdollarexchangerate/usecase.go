afb0fed68ce1b9ba8268f4dc4dcd1de550879108
Daniel <daniel.pedroso94@gmail.com>
2025-07-27 21:35:00 +0000

    feat: saving dollar quote into file

--- client/internal/usecase/getdollarexchangerate/usecase.go
+++ client/internal/usecase/getdollarexchangerate/usecase.go
@@ -3,6 +3,7 @@ package getdollarexchangerate
 import (
 	"context"
 	"fmt"
+	"os"
 )
 
 type UseCase interface {
@@ -29,6 +30,19 @@ func (u *usecase) Execute(ctx context.Context) (float32, error) {
 	quote, err := u.economiaAPIGateway.GetDollarExchangeRate(ctx)
 	if err != nil {
 		fmt.Printf("error recovering dolla exchange rate. [Error: %s]\n", err.Error())
+		return 0, err
+	}
+
+	f, err := os.OpenFile("arquivo.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Printf("error creating file. [Error: %s]\n", err.Error())
+		return 0, err
+	}
+	defer f.Close()
+
+	_, err = fmt.Fprintf(f, "Dólar: %f\n", quote)
+	if err != nil {
+		fmt.Printf("error writing to file. [Error: %s]\n", err.Error())
 		return 0, err
 	}
 
